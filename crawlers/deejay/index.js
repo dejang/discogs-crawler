9a31f9ac83cfa4f2f954ab89fd8a65b7c08906b8
Dejan <dgitin@Dejans-MacBook-Pro.local>
2018-08-26 21:31:35 -0700

    better deejay urls

--- crawlers/deejay/index.js
+++ crawlers/deejay/index.js
@@ -1,33 +1,34 @@
 class Deejay {
-    async get(item) {
-        const track = {};
-        track.title = item.trackTitle;
-        track.release_id = item.releaseId
-        track.deejay = `https://www.deejay.de/${item.catno}`
+  async get (item) {
+    const track = {};
+    track.title = item.trackTitle;
+    track.release_id = item.releaseId;
+    const catno = item.catno.replace (/\s+/g, '');
+    track.deejay = `https://www.deejay.de/${catno}`;
 
-        return [track];
-    }
+    return [track];
+  }
 }
 
-setTimeout(async() => {
-    const queue = [];
-    const shouldClose = false;
-    const crawler = new Deejay();
-    process.on('message', async(msg) => {
-        queue.push(msg);
-    });
+setTimeout (async () => {
+  const queue = [];
+  const shouldClose = false;
+  const crawler = new Deejay ();
+  process.on ('message', async msg => {
+    queue.push (msg);
+  });
 
-    async function run() {
-        const entry = queue.shift();
-        if (!entry) {
-            console.log('Deejay idle');
-            setTimeout(run, 30000);
-            return;
-        }
-        const returned = await crawler.get(entry);
-        process.send({ tracks: returned, size: queue.length });
-        setTimeout(run, 300);
+  async function run () {
+    const entry = queue.shift ();
+    if (!entry) {
+      console.log ('Deejay idle');
+      setTimeout (run, 30000);
+      return;
     }
+    const returned = await crawler.get (entry);
+    process.send ({tracks: returned, size: queue.length});
+    setTimeout (run, 300);
+  }
 
-    await run();
-}, 1)
+  await run ();
+}, 1);
